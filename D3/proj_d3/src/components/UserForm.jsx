45a73eefe3d3f66e9385106195394ee4f7896c40
SiMoZ9 <simonejonas08@gmail.com>
2023-09-11 15:53:09 +0200

    d3-todo

--- D3/proj_d3/src/components/UserForm.jsx
+++ D3/proj_d3/src/components/UserForm.jsx
@@ -9,84 +9,73 @@ class UserForm extends Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            FirstName: '',
+
+            LastName: '',
+            email: ''
+        };
+    }
+
+    handleInputChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const { FirstName, LastName, email } = this.state;
+        // POST invia questo come payload
+        console.log("Done")
     }
 
     render() {
-  return (
-    <Form>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="4" controlId="validationCustom01">
-          <Form.Label>First name</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="First name"
-            defaultValue="Mark"
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="4" controlId="validationCustom02">
-          <Form.Label>Last name</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Last name"
-            defaultValue="Otto"
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-          <Form.Label>Username</Form.Label>
-          <InputGroup hasValidation>
-            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-            <Form.Control
-              type="text"
-              placeholder="Username"
-              aria-describedby="inputGroupPrepend"
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              Please choose a username.
-            </Form.Control.Feedback>
-          </InputGroup>
-        </Form.Group>
-      </Row>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="6" controlId="validationCustom03">
-          <Form.Label>City</Form.Label>
-          <Form.Control type="text" placeholder="City" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid city.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom04">
-          <Form.Label>State</Form.Label>
-          <Form.Control type="text" placeholder="State" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid state.
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="3" controlId="validationCustom05">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control type="text" placeholder="Zip" required />
-          <Form.Control.Feedback type="invalid">
-            Please provide a valid zip.
-          </Form.Control.Feedback>
-        </Form.Group>
-      </Row>
-      <Form.Group className="mb-3">
-        <Form.Check
-          required
-          label="Agree to terms and conditions"
-          feedback="You must agree before submitting."
-          feedbackType="invalid"
-        />
-      </Form.Group>
-      <Button type="submit">Submit form</Button>
-    </Form>
-  );
+        console.log(this.state)
+        return (
+            <Form onSubmit={this.handleSubmit}>
+                <Row className="mb-3">
+                    <Form.Group as={Col} md="4" controlId="validationCustom01">
+                        <Form.Label>First name</Form.Label>
+                        <Form.Control
+                            required
+                            name='FirstName'
+                            value={this.state.FirstName}
+                            type="text"
+                            placeholder="First name"
+                            onChange={this.handleInputChange}
+                        />
+                    </Form.Group>
+                    <Form.Group as={Col} md="4" controlId="validationCustom02">
+                        <Form.Label>Last name</Form.Label>
+                        <Form.Control
+                            required
+                            name='LastName'
+                            value={this.state.LastName}
+                            type="text"
+                            placeholder="Last name"
+                            onChange={this.handleInputChange}
+                        />
+                    </Form.Group>
+                    <Form.Group as={Col} md="4" controlId="validationCustom05">
+                        <Form.Label>email</Form.Label>
+                        <Form.Control
+                            required
+                            name='email'
+                            value={this.state.email}
+                            type="email"
+                            placeholder="email"
+                            onChange={this.handleInputChange}
+                        />
+                    </Form.Group>
 
-  }
+                </Row>
+
+                <Button type="submit">Submit form</Button>
+            </Form>
+        );
+
+    }
 }
 
 export default UserForm;
